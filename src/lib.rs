f06a79ffeec4f6bf080042514e07ba957741d402
quininer kel <quininer@live.com>
2016-06-06 13:46:38 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+#![no_std]
+
+extern crate byteorder;
+
+mod util;
+mod ops;
+
+pub use ops::Hc256Rng;
+
+
+/// ```
+/// use hc256::HC256;
+/// let mut output = [0; 32];
+///
+/// HC256::new(&[0; 32], &[0; 32]).process(&[0; 32], &mut output);
+/// assert_eq!(
+///     output,
+///     [
+///         0x5b, 0x07, 0x89, 0x85, 0xd8, 0xf6, 0xf3, 0x0d,
+///         0x42, 0xc5, 0xc0, 0x2f, 0xa6, 0xb6, 0x79, 0x51,
+///         0x53, 0xf0, 0x65, 0x34, 0x80, 0x1f, 0x89, 0xf2,
+///         0x4e, 0x74, 0x24, 0x8b, 0x72, 0x0b, 0x48, 0x18
+///     ]
+/// );
+///
+/// let mut iv = [0; 32];
+/// iv[0] = 1;
+/// HC256::new(&[0; 32], &iv).process(&[0; 32], &mut output);
+/// assert_eq!(
+///     output,
+///     [
+///         0xaf, 0xe2, 0xa2, 0xbf, 0x4f, 0x17, 0xce, 0xe9,
+///         0xfe, 0xc2, 0x05, 0x8b, 0xd1, 0xb1, 0x8b, 0xb1,
+///         0x5f, 0xc0, 0x42, 0xee, 0x71, 0x2b, 0x31, 0x01,
+///         0xdd, 0x50, 0x1f, 0xc6, 0x0b, 0x08, 0x2a, 0x50
+///     ]
+/// );
+///
+/// let mut key = [0; 32];
+/// key[0] = 0x55;
+/// HC256::new(&key, &[0; 32]).process(&[0; 32], &mut output);
+/// assert_eq!(
+///     output,
+///     [
+///         0x1c, 0x40, 0x4a, 0xfe, 0x4f, 0xe2, 0x5f, 0xed,
+///         0x95, 0x8f, 0x9a, 0xd1, 0xae, 0x36, 0xc0, 0x6f,
+///         0x88, 0xa6, 0x5a, 0x3c, 0xc0, 0xab, 0xe2, 0x23,
+///         0xae, 0xb3, 0x90, 0x2f, 0x42, 0x0e, 0xd3, 0xa8
+///     ]
+/// );
+/// ```
+pub struct HC256 {
+    inner: Hc256Rng,
+    buff: [u8; 4],
+    index: usize
+}
+
+impl HC256 {
+    pub fn new(key: &[u8], iv: &[u8]) -> HC256 {
+        let mut ukey = [0; 8];
+        let mut uiv = [0; 8];
+        util::u8_to_u32(key, &mut ukey);
+        util::u8_to_u32(iv, &mut uiv);
+
+        HC256 {
+            inner: Hc256Rng::init(&ukey, &uiv),
+            buff: [0; 4],
+            index: 4
+        }
+    }
+
+    pub fn process(&mut self, input: &[u8], output: &mut [u8]) {
+        for (i, k) in self.take(input.len()).enumerate() {
+            output[i] = input[i] ^ k;
+        }
+    }
+}
+
+impl Iterator for HC256 {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.buff.len() {
+            util::u32_to_u8(self.inner.gen(), &mut self.buff);
+            self.index = 0;
+            self.next()
+        } else {
+            let output = self.buff[self.index];
+            self.index += 1;
+            Some(output)
+        }
+    }
+}
---
+++ src/ops.rs
@@ -0,0 +1,96 @@
+/// ```
+/// use hc256::Hc256Rng;
+/// assert_eq!(
+///     Hc256Rng::init(&[0; 8], &[0; 8]).gen(),
+///     2240350043
+/// );
+/// ```
+pub struct Hc256Rng {
+    pub p: [u32; 1024],
+    pub q: [u32; 1024],
+    pub c: u32
+}
+
+impl Hc256Rng {
+    pub fn init(key: &[u32], iv: &[u32]) -> Hc256Rng {
+        let mut w = [0; 2560];
+        let mut hc256 = Hc256Rng {
+            p: [0; 1024],
+            q: [0; 1024],
+            c: 0
+        };
+
+        for i in 0..8 {
+            w[i] = key[i];
+        }
+        for i in 8..16 {
+            w[i] = iv[i - 8];
+        }
+        for i in 16..2560 {
+            w[i] = f2(w[i - 2])
+                .wrapping_add(w[i - 7])
+                .wrapping_add(f1(w[i - 15]))
+                .wrapping_add(w[i - 16])
+                .wrapping_add(i as u32);
+        }
+        for i in 0..1024 {
+            hc256.p[i] = w[i + 512];
+            hc256.q[i] = w[i + 1536];
+        }
+
+
+        for _ in 0..4096 {
+            hc256.gen();
+        }
+
+        hc256
+    }
+
+    pub fn gen(&mut self) -> u32 {
+        let i = (self.c & 0x3ff) as usize;
+        let i3 = i.wrapping_sub(3) & 0x3ff;
+        let i10 = i.wrapping_sub(10) & 0x3ff;
+        let i12 = i.wrapping_sub(12) & 0x3ff;
+        let i1023 = i.wrapping_sub(1023) & 0x3ff;
+
+        let output = if self.c < 1024 {
+            self.p[i] = self.p[i]
+                .wrapping_add(self.p[i10])
+                .wrapping_add(self.p[i3].rotate_right(10) ^ self.p[i1023].rotate_right(23))
+                .wrapping_add(self.q[(self.p[i3] ^ self.p[i1023]) as usize & 0x3ff]);
+            h(&self.q, self.p[i12]) ^ self.p[i]
+        } else {
+            self.q[i] = self.q[i]
+                .wrapping_add(self.q[i10])
+                .wrapping_add(self.q[i3].rotate_right(10) ^ self.q[i1023].rotate_right(23))
+                .wrapping_add(self.p[(self.q[i3] ^ self.q[i1023]) as usize & 0x3ff]);
+            h(&self.p, self.q[i12]) ^ self.q[i]
+        };
+
+        self.c = (self.c + 1) & 0x7ff;
+        output
+    }
+}
+
+
+#[inline]
+fn h(q: &[u32], u: u32) -> u32 {
+    q[(u & 0xff) as usize]
+        .wrapping_add(q[256 + (u >> 8 & 0xff) as usize])
+        .wrapping_add(q[512 + (u >> 16 & 0xff) as usize])
+        .wrapping_add(q[768 + (u >> 24 & 0xff) as usize])
+}
+
+#[inline]
+fn f1(x: u32) -> u32 {
+    x.rotate_right(7)
+        ^ x.rotate_right(18)
+        ^ x.wrapping_shr(3)
+}
+
+#[inline]
+fn f2(x: u32) -> u32 {
+    x.rotate_right(17)
+        ^ x.rotate_right(19)
+        ^ x.wrapping_shr(10)
+}
